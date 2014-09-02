30be0397da2da9fcbb8deb96691c6d305fce7788
Håvard Pettersson <haavard.pettersson@gmail.com>
2014-09-02 23:57:06 +0200

    Fixed /msg sending recipient as part of the message.

--- src/tox-weechat-commands.c
+++ src/tox-weechat-commands.c
@@ -278,7 +278,7 @@ tox_weechat_cmd_msg(void *data, struct t_gui_buffer *buffer,
         tox_send_message(tox,
                          friend_number,
                          (uint8_t *)argv_eol[1],
-                         strlen(argv_eol[1]));
+                         strlen(argv_eol[2]));
         char *name = tox_weechat_get_self_name_nt();
         tox_weechat_chat_print_action(chat, name, argv_eol[1]);
         free(name);
