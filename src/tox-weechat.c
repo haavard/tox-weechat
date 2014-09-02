8fdcc5173f9c7756ab28797e309f3d407856b96d
Håvard Pettersson <haavard.pettersson@gmail.com>
2014-09-02 13:59:38 +0200

    Stuff.

--- src/tox-weechat.c
+++ src/tox-weechat.c
@@ -1,4 +1,10 @@
+#include <stdlib.h>
+
 #include <weechat/weechat-plugin.h>
+
+#include "tox-weechat-tox.h"
+
+#include "tox-weechat.h"
 
 WEECHAT_PLUGIN_NAME("tox");
 WEECHAT_PLUGIN_DESCRIPTION("Tox protocol");
@@ -6,10 +12,15 @@ WEECHAT_PLUGIN_AUTHOR("Håvard Pettersson <haavard.pettersson@gmail.com>");
 WEECHAT_PLUGIN_VERSION("0.1");
 WEECHAT_PLUGIN_LICENSE("GPL3");
 
+struct t_weechat_plugin *weechat_plugin = NULL;
+
 int
-weechat_plugin_init(struct t_weechat_plugin *plugin,
-                    int argc, char *argv[])
+weechat_plugin_init(struct t_weechat_plugin *plugin, int argc, char *argv[])
 {
+    weechat_plugin = plugin;
+
+    tox_weechat_tox_init();
+
     return WEECHAT_RC_OK;
 }
 
--- src/tox-weechat.h
+++ src/tox-weechat.h
@@ -1,4 +1,6 @@
 #ifndef TOX_WEECHAT_H
 #define TOX_WEECHAT_H
 
+extern struct t_weechat_plugin *weechat_plugin;
+
 #endif // TOX_WEECHAT_H
