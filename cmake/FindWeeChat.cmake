334690f40f2810fa5851306ad49c7f4daad75f36
Håvard Pettersson <mail@haavard.me>
2017-02-03 16:03:47 -0800

    Formatting and .gitignore

--- cmake/FindWeeChat.cmake
+++ cmake/FindWeeChat.cmake
@@ -20,4 +20,6 @@
 find_path(WeeChat_INCLUDE_DIRS weechat/weechat-plugin.h)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(WeeChat FOUND_VAR WeeChat_FOUND REQUIRED_VARS WeeChat_INCLUDE_DIRS)
+find_package_handle_standard_args(WeeChat
+                                  FOUND_VAR WeeChat_FOUND
+                                  REQUIRED_VARS WeeChat_INCLUDE_DIRS)
